c8e24c086cf28bc7987a786f95ecd34ae67f2ebb
cthulhu-engineer <cthulhu.engineer@gmail.com>
2024-02-04 15:15:59 +0200

    Refactor Monte Carlo method and update Readme.md & requirements
    Refactors the Monte Carlo simulation in task_2.py to use numpy, and provide results for various point numbers. The Readme.md file has been updated to reflect these changes and provide more detailed results and explanation. Additionally, 'numpy' has been added to the list of project requirements in the requirements.txt file.

--- task_2.py
+++ task_2.py
@@ -1,31 +1,37 @@
-import random
-import scipy.integrate as spi
+import numpy as np
+from scipy.integrate import quad
 
 
-def monte_carlo_integration(func, a, b, n):
-    total_area = 0
-    for _ in range(n):
-        x = random.uniform(a, b)
-        y = random.uniform(0, 2)
-        if y <= func(x):
-            total_area += 1
-    return total_area / n * (b - a)
+def monte_carlo(num_points):
+    random_x = np.random.uniform(a, b, num_points)
+    random_y = np.random.uniform(0, f(b), num_points)
+    # Обчислення кількості точок, які потрапили під криву
+    points_under_curve = sum(random_y <= f(random_x))
+    # Обчислення відсотка площі під кривою
+    area_ratio = points_under_curve / num_points
+    # Обчислення площі області
+    total_area = (b - a) * f(b)
+    # Обчислення значення інтегралу
+    integral_value = total_area * area_ratio
+
+    return integral_value
 
 
 # Функція, для якої ми обчислюємо інтеграл
-def sample_function(x):
+def f(x):
     return x ** 3
 
 
 # Параметри для обчислення інтеграла
 a = 0
 b = 2
-n = 1000000
+# Обчислення інтеграла за допомогою методу Монте-Карло
+num_points = [10000, 20000, 50000, 10, 100, 1000]
 
-# Обчислення інтеграла методом Монте-Карло
-monte_carlo_result = monte_carlo_integration(sample_function, a, b, n)
-print("Monte Carlo Integration Result:", monte_carlo_result)
+analytical_solution_quad, _ = quad(f, a, b)
 
-# Обчислення інтеграла за допомогою quad
-quad_result, _ = spi.quad(sample_function, a, b)
-print("Quad Integration Result:", quad_result)
+print("Обчислення інтеграла за допмогою метод Монте-Карло: ")
+for points in num_points:
+    estimate = monte_carlo(points)
+    error = np.abs(estimate - analytical_solution_quad)
+    print(f"Кількість точок: {points}, інтеграл: {estimate}, помилка: {error}")
